d6229377b9a87b9678abf090544ed54ab675efc0
manshagupta217 <mansha.g217@gmail.com>
2025-12-17 18:10:04 +0000

    Strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -7,12 +7,12 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);  // u
 
 const gameName = new String('hello-world')
 
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
+console.log(gameName[0]);
+console.log(gameName.__proto__);
 
 
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
 
@@ -35,3 +35,4 @@ console.log(url.replace('%20', '-'))
 console.log(url.includes('sundar'))
 
 console.log(gameName.split('-'));
+
