55c6d485001880a9ff5a080feea4f73c8337ec4d
manshagupta217 <mansha.g217@gmail.com>
2025-12-23 06:37:40 +0000

    objects 2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -29,7 +29,7 @@ const obj2={3: "a", 4:"b"}
 // const obj3={obj1,obj2}
 // const obj3= Object.assign({},obj1,obj2)
 const obj3={...obj1, ...obj2}
-console.log(obj3);
+// console.log(obj3);
 
 
 const users=[
@@ -51,19 +51,39 @@ const users=[
     }
 ]
 
-users[1].email
-console.log(tinderUser);
-console.log(Object.keys(tinderUser)); //saari keys ek array mai aajaegi
-console.log(Object.values(tinderUser)); //saare values ek array mai aajaenge
-console.log(Object.entries(tinderUser)); //har ek key value ko array mai bna diya jaata hai
+// users[1].email
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser)); //saari keys ek array mai aajaegi
+// console.log(Object.values(tinderUser)); //saare values ek array mai aajaenge
+// console.log(Object.entries(tinderUser)); //har ek key value ko array mai bna diya jaata hai
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
-
-
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
 
+//destructuring
 
+const course={
+    coursename:"js in hindi",
+    price: "999",
+    courseInstructor:"hitesh"
+}
+
+//course.courseInstructor
+
+const {courseInstructor : instructor} = course
+// console.log(courseInstructor);
+
+console.log(instructor);
+
+
+
+// json api (js object notation)
+// {
+//     "name":"hitesh",
+//     "coursename":"js in hindi",
+//     "price":"free"
+// }
 
 
 
