3e16ed66a0b4e2138860a86cdb140e799fd89c74
manshagupta217 <mansha.g217@gmail.com>
2025-12-27 04:36:52 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,37 @@
+function sayMyName(){
+    console.log("M");
+    console.log("A");
+    console.log("N");
+    console.log("S");
+    console.log("H");
+    console.log("A");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(num1,num2){  //num1 and num2 are parameters
+//     console.log(num1+num2);
+    
+// }
+
+// addTwoNumbers(3,"4") // 34 (3 and 4 are arguments)
+// addTwoNumbers(3,"a") // 3a
+// addTwoNumbers(3,null) // 3
+
+// const result = addTwoNumbers(3,5) // yaha result ki value undefined aayegi kyuki ans result mai strore ni ho rha hai
+
+
+// console.log("Result: ",result);
+
+function addTwoNumbers(num1,num2){  
+    
+    // let result = num1+num2
+    // return result // ek baar function mai return hone ke baad function koi bhi kam ni krega
+    // console.log("hello");  // ye print ni hoga
+
+    return num1+num2
+    
+}
+
+const result =  addTwoNumbers(3,5) // output will be 8
+console.log("Result: ",result);
