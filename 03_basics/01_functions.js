a745e86e985c0a6a91fe37528f4b0609acabcab1
manshagupta217 <mansha.g217@gmail.com>
2025-12-27 04:59:27 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -39,7 +39,7 @@ const result =  addTwoNumbers(3,5) // output will be 8
 
 function loginUserMessage(username){
     if(username == undefined){
-        console.log("Please enter a username");
+        // console.log("Please enter a username");
         return
         
     }
@@ -48,4 +48,38 @@ function loginUserMessage(username){
 
 // console.log(loginUserMessage("Mansha"))
 
-console.log(loginUserMessage());  // agr koi bhi argument ni diya toh undefined print hoga
+// console.log(loginUserMessage());  // agr koi bhi argument ni diya toh undefined print hoga
+
+// ... -> rest operator(pass multiple values in function)
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200,400,500,2000));
+
+const user = {
+    username: "mansha",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200,400,100,500]
+
+function returnSecondValue(getarray){
+    return getarray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,100,1000]));
+
