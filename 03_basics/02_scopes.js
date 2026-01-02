9b4950869e2b276f01c31b1ed1cad5686225b702
manshagupta217 <mansha.g217@gmail.com>
2026-01-02 13:10:41 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,16 +7,44 @@ if(true){
     const b = 20 //b is readable ONLY inside the if block, just like a.
     // var c = 30 // avoid declaring var
 
-    console.log("INNER: ",a);
+    // console.log("INNER: ",a);
     // console.log(b);
     
 
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
 // inspect krke core scope is different and in coding environment global scope is different
 
 
+// nested scope
+
+function one(){
+    const username= " mansha "
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);  //o/p: website is not defined
+
+    // two() //o/p: mansha
+    
+}
+
+// one() //o/p: nothing will print
+
+// same concept with if and else
+
+if (true) {
+    const username = "mansha"
+    if(username === "mansha"){
+        const website = " youtube "
+        console.log(username + website);
+        
+    }
+}
